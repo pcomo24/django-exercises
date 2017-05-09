a0090f0b51fbbd5972b2345e20938d6d0255e6eb
pcomo24 <pcomo24@gmail.com>
2017-05-09 12:14:00 -0500

    settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '104.238.146.151'
+]
 
 
 # Application definition
